ecddda806ac5506ee3f47a62527123fec431f99c
Noel Mendoza <nmendozavd@gmail.com>
2018-02-26 01:04:20 -0800

    initial commit

---
+++ basic_hashing.py
@@ -0,0 +1,10 @@
+import md5 # do this at the top of your file where you import modules
+@app.route('/users/create', methods=['POST'])
+def create_user():
+     username = request.form['username']
+     email = request.form['email']
+     password = md5.new(request.form['password']).hexdigest()
+     insert_query = "INSERT INTO users (username, email, password, created_at, updated_at) VALUES (:username,
+     :email, :password, NOW(), NOW())"
+     query_data = { 'username': username, 'email': email, 'password': password }
+     mysql.query_db(insert_query, query_data)
